32a6681cec8c9f3b28339f281c5bb4af9b92e16b
Andrew <andrewzlatterson@gmail.com>
2022-06-11 12:17:16 +0100

    finished all mvp repository functions

--- console.py
+++ console.py
@@ -12,6 +12,7 @@ import repositories.transaction_repository as transaction_repository
 user_repository.delete_all()
 merchant_repository.delete_all()
 item_repository.delete_all()
+transaction_repository.delete_all()
 
 user1 = User("Todd",10,20)
 user_repository.save(user1)
@@ -76,7 +77,7 @@ print(specific_item.name, specific_item.id, specific_item.merchant.name, specifi
 
 # specific item working
 
-item2= Item("yoyo","weapon",10,merchant2,item2.id)
+item2= Item("yoyo","weapon",50,merchant2,item2.id)
 item_repository.update(item2)
 
 items = item_repository.select_all()
@@ -92,11 +93,29 @@ for user in users:
 
 
 print(merchant2,item2,user2,item2.cost)
-transaction1 = Transaction(merchant2,item2,user2,item2.cost)
+transaction1 = Transaction(merchant1.id,item2.id,user2.id,item2.cost)
 transaction_repository.save(transaction1)
 
+transaction2 = Transaction(merchant2.id,item1.id,user2.id,item1.cost)
+transaction_repository.save(transaction2)
+
+# -- save transactions: working
+
+transaction_list = transaction_repository.select_all()
+for transaction in transaction_list:
+    print(transaction.merchant.name, transaction.item.name, transaction.user.name, transaction.cost)
+
+# -- select all transactions: working
 
 
+single_transaction = transaction_repository.select(transaction1.id)
+print(single_transaction.id, single_transaction.cost, single_transaction.merchant.name, single_transaction.user.name)
+# -- select single transaction: working
+
+transaction_repository.delete(transaction1.id)
+transaction_list = transaction_repository.select_all()
+for transaction in transaction_list:
+    print(transaction.merchant.name, transaction.item.name, transaction.user.name, transaction.cost)
 
 
 pdb.set_trace()
--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -1,6 +1,8 @@
+from unittest import result
 from db.run_sql import run_sql
 
 from models.merchant import Merchant
+from models.transaction import Transaction
 
 import repositories.merchant_repository as merchant_repository
 import repositories.user_repository as user_repository
@@ -8,7 +10,39 @@ import repositories.item_repository as item_repository
 
 def save(transaction):
     sql = "INSERT INTO transactions (merchant_id, item_id, user_id, cost) VALUES (%s,%s,%s,%s) RETURNING id"
-    values = [transaction.merchant.id,transaction.item.id, transaction.user.id, transaction.cost]
+    values = [transaction.merchant,transaction.item, transaction.user, transaction.cost]
     results = run_sql(sql,values)
     transaction.id = results[0]['id']
     return transaction
+
+def select_all():
+    transactions = []
+    sql = "SELECT * FROM transactions"
+    results = run_sql(sql)
+    for row in results:
+        merchant = merchant_repository.select(row["merchant_id"])
+        item = item_repository.select(row["item_id"])
+        user = user_repository.select(row["user_id"])
+        transaction = Transaction(merchant,item,user,row["cost"])
+        transactions.append(transaction)
+    return transactions
+
+def select(id):
+    sql = "SELECT * FROM transactions WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    merchant = merchant_repository.select(result["merchant_id"])
+    item = item_repository.select(result["item_id"])
+    user = user_repository.select(result["user_id"])
+    transaction = Transaction(merchant,item,user,result["cost"])
+    return transaction
+
+
+def delete_all():
+    sql = "DELETE FROM transactions"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM transactions WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
